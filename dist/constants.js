651ecd9287d8635ed43927d6eca367080e88f7f9
Andrew Ewing <drew@phenocode.com>
2020-09-15 15:40:59 -0700

    added svelte extension

--- dist/constants.js
+++ dist/constants.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.GITHUB_URL = exports.EXTENSIONS_TO_LINT = exports.CHECK_NAME = void 0;
-const CHECK_NAME = 'ESLint check';
+const CHECK_NAME = "ESLint check";
 exports.CHECK_NAME = CHECK_NAME;
-const EXTENSIONS_TO_LINT = ['.mjs', '.js', '.ts', '.jsx', '.tsx'];
+const EXTENSIONS_TO_LINT = [".mjs", ".js", ".ts", ".jsx", ".tsx", ".svelte"];
 exports.EXTENSIONS_TO_LINT = EXTENSIONS_TO_LINT;
-const GITHUB_URL = 'github.com';
+const GITHUB_URL = "github.com";
 exports.GITHUB_URL = GITHUB_URL;
--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,12 @@
-export const CHECK_NAME = 'ESLint check';
+export const CHECK_NAME = "ESLint check";
 
 export const EXTENSIONS_TO_LINT = [
-  '.mjs',
-  '.js',
-  '.ts',
-  '.jsx',
-  '.tsx'
+  ".mjs",
+  ".js",
+  ".ts",
+  ".jsx",
+  ".tsx",
+  ".svelte",
 ];
 
-export const GITHUB_URL = 'github.com';
+export const GITHUB_URL = "github.com";
